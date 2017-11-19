a084fa786e64211c35fb06680ff4aa3f268e7588
Armando Brito <chynomon@gmail.com>
2017-11-19 19:18:14 +0000

    Criação da classe PublicTest e o ejb.

---
+++ MasterManager-ejb/src/java/ejbs/PublicTestBean.java
@@ -0,0 +1,83 @@
+package ejbs;
+
+import entities.PublicTest;
+import exceptions.EntityAlreadyExistsException;
+import exceptions.EntityDoesNotExistException;
+import java.util.Date;
+import java.util.List;
+import javax.ejb.EJBException;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Stateless
+public class PublicTestBean {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public void create(int code,String title, Date testDate,
+            String testHour, String place, String link) throws EntityAlreadyExistsException,
+            EntityDoesNotExistException {
+        try {
+            if (em.find(PublicTest.class, code) != null) {
+                throw new EntityAlreadyExistsException(
+                        "Uma prova publica já existe com esse código.");
+            }
+            em.persist(new PublicTest(code, title, testDate, testHour, 
+                    place, link));
+        } catch (EntityAlreadyExistsException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new EJBException(e.getMessage());
+        }
+    }
+    
+    public void update(int code,String title, Date testDate, String testHour, 
+            String place, String link) throws EntityDoesNotExistException {
+        try {
+            PublicTest publicTest = em.find(PublicTest.class, code);
+            if (publicTest == null) {
+                throw new EntityDoesNotExistException(
+                        "Não existe nenhuma prova publica com esse código.");
+            }
+            
+            publicTest.setTitle(title);
+            publicTest.setTestDate(testDate);
+            publicTest.setTestHour(testHour);
+            publicTest.setPlace(place);
+            publicTest.setLink(link);
+            
+            em.merge(publicTest);
+        } catch (EntityDoesNotExistException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new EJBException(e.getMessage());
+        }
+    }
+    
+    public void remove (int code) throws EntityDoesNotExistException{
+        try {
+            PublicTest publicTest = em.find(PublicTest.class, code);
+            if (publicTest == null) {
+                throw new EntityDoesNotExistException(
+                        "Não existe nenhuma prova publica com esse código.");
+            }
+            
+            em.remove(publicTest);
+        } catch (EntityDoesNotExistException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new EJBException(e.getMessage());
+        }
+    }
+    
+    public List<PublicTest> getAll(){
+        try {
+            List<PublicTest> publicTests = (List<PublicTest>) em.createNamedQuery("getAllPublicTests").getResultList();
+            return publicTests;
+        } catch (Exception e) {
+            throw new EJBException(e.getMessage());
+        }
+    }
+}
---
+++ MasterManager-ejb/src/java/entities/PublicTest.java
@@ -0,0 +1,134 @@
+package entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "PUBLIC_TESTS")
+@NamedQuery(name = "getAllPublicTests",
+        query = "SELECT s FROM PublicTest s ORDER BY s.testDate, s.testHour")
+public class PublicTest implements Serializable {
+
+    @Id
+    private int code;
+    
+    @NotNull
+    private String title;
+    
+    @NotNull
+    @Temporal(TemporalType.DATE)
+    @Column(name = "TEST_DATE")
+    private Date testDate;
+    
+    @NotNull
+    @Column(name = "TEST_HOUR")
+    private String testHour;
+    
+    @NotNull
+    private String place;
+    
+    @NotNull
+    private String link;
+    
+    @NotNull
+    //@OneToMany
+    private List<Student> students;
+    
+    private List<Jury> jury;
+    
+    
+
+    public PublicTest() {
+        jury = new LinkedList<>();
+        students = new LinkedList<>();
+    }
+
+    public PublicTest(int code,String title, Date testDate,
+            String testHour, String place, String link) {
+        this.code = code;
+        this.title = title;
+        this.testDate = testDate;
+        this.testHour = testHour;
+        this.place = place;
+        this.link = link;
+        jury = new LinkedList<>();
+        students = new LinkedList<>();
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getTestDate() {
+        return testDate;
+    }
+
+    public void setTestDate(Date testDate) {
+        this.testDate = testDate;
+    }
+
+    public String getTestHour() {
+        return testHour;
+    }
+
+    public void setTestHour(String testHour) {
+        this.testHour = testHour;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public List<Jury> getJury() {
+        return jury;
+    }
+
+    public void setJury(List<Jury> jury) {
+        this.jury = jury;
+    }
+
+    public List<Student> getStudent() {
+        return students;
+    }
+
+    public void setStudent(List<Student> student) {
+        this.students = student;
+    }
+    
+    
+
+}
