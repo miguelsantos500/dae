e5a3ce919659256ead828d2110e3efe4b6182962
Armando Brito <chynomon@gmail.com>
2017-12-03 16:52:17 +0000

    Página para criar uma prova publica

---
+++ MasterManager-war/src/java/web/AdministratorManager.java
@@ -0,0 +1,147 @@
+package web;
+
+import dtos.CourseDTO;
+import dtos.PublicTestDTO;
+import dtos.StudentDTO;
+import ejbs.PublicTestBean;
+import ejbs.users.CCPUserBean;
+import ejbs.users.InstitutionBean;
+import ejbs.users.StudentBean;
+import ejbs.users.TeacherBean;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Logger;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIComponent;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+
+@ManagedBean
+@SessionScoped
+public class AdministratorManager {
+
+    @EJB
+    private CCPUserBean ccpUserBean;
+    @EJB
+    private InstitutionBean institutionBean;
+    @EJB
+    private StudentBean studentBean;
+    @EJB
+    private TeacherBean teacherBean;
+    @EJB
+    private PublicTestBean publicTestBean;
+
+    private StudentDTO newStudent;
+    private CourseDTO newCourse;
+    private PublicTestDTO newPublicTest;
+    private UIComponent component;
+    private static final Logger logger = Logger.getLogger("web.AdministratorManager");
+    
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    private String dateNow = dtf.format(LocalDate.now());
+
+    private Client client;
+    private final String baseUri
+            = "http://localhost:8080/MasterManager-war/webapi";
+
+    public AdministratorManager() {
+        newStudent = new StudentDTO();
+        newCourse = new CourseDTO();
+        newPublicTest = new PublicTestDTO();
+        client = ClientBuilder.newClient();
+    }
+
+    public String creaTeStudent() {
+
+        try {
+            studentBean.create(
+                    newStudent.getUsername(),
+                    newStudent.getPassword(),
+                    newStudent.getName(),
+                    newStudent.getEmail(),
+                    newStudent.getCourseCode());
+            newStudent.reset();
+
+        } catch (Exception e) {
+            FacesExceptionHandler.handleException(e, "Erro inesperado", component, logger);
+            return null;
+        }
+        return "index?faces-redirect=true";
+    }
+
+    public List<StudentDTO> getAllStudents() {
+        List<StudentDTO> returnedStudents = null;
+        try {
+            returnedStudents = client.target(baseUri)
+                    .path("/students/all")
+                    .request(MediaType.APPLICATION_XML)
+                    .get(new GenericType<List<StudentDTO>>() {
+                    });
+
+        } catch (Exception e) {
+            FacesExceptionHandler.handleException(e, "Erro inesperado", logger);
+
+        }
+        return returnedStudents;
+    }
+
+    ////////////// PUBLIC TEST ///////////////////
+    public String createPublicTest() {
+
+        try {
+            publicTestBean.create(
+                    newPublicTest.getCode(),
+                    newPublicTest.getTitle(),
+                    newPublicTest.getTestDateTime(),
+                    newPublicTest.getPlace(),
+                    newPublicTest.getLink(),
+                    newPublicTest.getCCPUserUsername(),
+                    newPublicTest.getTeacherUsername(),
+                    newPublicTest.getOutsideTeacherName(),
+                    newPublicTest.getOutsideTeacherEmail(),
+                    newPublicTest.getStudentUsername());
+            newPublicTest.reset();
+
+        } catch (Exception e) {
+            FacesExceptionHandler.handleException(e, "Erro ao criar a prova pública!", 
+                    component, logger);
+            return null;
+        }
+        return "index?faces-redirect=true";
+    }
+
+    public List<PublicTestDTO> getAllPublicTests() {
+        try {
+            return publicTestBean.getAll();
+        } catch (Exception e) {
+            FacesExceptionHandler.handleException(e, "Unexpected error! Try again latter!", 
+                    logger);
+            return null;
+        }
+    }
+
+    public PublicTestDTO getNewPublicTest() {
+        return newPublicTest;
+    }
+
+    public void setNewPublicTest(PublicTestDTO newPublicTest) {
+        this.newPublicTest = newPublicTest;
+    }
+
+    public String getDateNow() {
+        return dateNow;
+    }
+
+    public void setDateNow(String dateNow) {
+        this.dateNow = dateNow;
+    }
+    
+    
+    
+}
