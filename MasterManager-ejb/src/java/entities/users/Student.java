9f56522fcedf14a99f6205b7eb236d872b8a037a
Soraia Basso <soraiapratesbasso@gmail.com>
2017-12-23 12:30:04 +0000

    CRIERI associação  estudantes - project_proposals

--- MasterManager-ejb/src/java/entities/users/Student.java
+++ MasterManager-ejb/src/java/entities/users/Student.java
@@ -1,11 +1,15 @@
 package entities.users;
 
 import entities.Course;
+import entities.project.ProjectProposal;
 import entities.publictest.PublicTest;
 import entities.users.UserGroup.GROUP;
 import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
@@ -14,7 +18,7 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
 @Entity
-@Table( name = "STUDENTS")
+@Table(name = "STUDENTS")
 @NamedQueries({
     @NamedQuery(name = "getAllStudents",
     query = "SELECT s FROM Student s ORDER BY s.name")})
@@ -29,12 +33,17 @@ public class Student extends User implements Serializable {
     @JoinColumn(name = "PUBLIC_CODE")
     private PublicTest publicTest;
     
+    @ManyToMany(mappedBy = "students")
+    public List<ProjectProposal> projectProposals;
+    
     public Student() {
+        this.projectProposals = new LinkedList<>();
     }
 
     public Student(String username, String password, String name, String email, Course course) {
         super(username, password, GROUP.Student, name, email);
         this.course = course;
+        this.projectProposals = new LinkedList<>();
     }
 
     public Course getCourse() {
